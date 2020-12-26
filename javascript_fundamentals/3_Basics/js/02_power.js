e16c692bedb7cc05609ae4feb0e21d59790d7498
Lena Blechmann <lenablechmann@gmail.com>
2020-12-26 16:46:07 +0100

    Add error check for non positive ints

--- javascript_fundamentals/3_Basics/js/02_power.js
+++ javascript_fundamentals/3_Basics/js/02_power.js
@@ -3,6 +3,11 @@ function calc_power() {
   const number1 = Number(document.getElementById("user_num_1").value);
   const number2 = Number(document.getElementById("user_num_2").value);
   const paragraph = document.querySelector("p");
+  if (number2 < 1)
+  {
+    paragraph.textContent = "Only positive integers are supported for n."
+    return;
+  }
   let result = number1;
   // A loop that multiplies the number with itself n times
   for (let counter = 0; counter < number2 - 1; counter++)
