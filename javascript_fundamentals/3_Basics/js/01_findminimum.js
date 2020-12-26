28657ae6f57bac6a62f3d095372ce0e06cfb1ee6
Lena Blechmann <lenablechmann@gmail.com>
2020-12-26 16:42:44 +0100

    Add an html + js that calculates x to the power of n

--- javascript_fundamentals/3_Basics/js/01_findminimum.js
+++ javascript_fundamentals/3_Basics/js/01_findminimum.js
@@ -1,17 +1,13 @@
 function find_min() {
-    // saving user inputs into variables.
-    const number1 = Number(document.getElementById("user_num_1").value);
-    const number2 = Number(document.getElementById("user_num_2").value);
-    const paragraph = document.querySelector("p");
-    let minimum = 0;
-    if (number1 === number2)
-    {
+  // saving user inputs into variables.
+  const number1 = Number(document.getElementById("user_num_1").value);
+  const number2 = Number(document.getElementById("user_num_2").value);
+  const paragraph = document.querySelector("p");
+  let minimum = 0;
+  if (number1 === number2) {
     paragraph.textContent = "The two numbers are equal!";
-    }
-    else
-    {
-    (number1 < number2) ? minimum = number1 : minimum = number2;
+  } else {
+    number1 < number2 ? (minimum = number1) : (minimum = number2);
     paragraph.textContent = `The smallest number is: ${minimum}`;
-    }
-
+  }
 }
---
+++ javascript_fundamentals/3_Basics/js/02_power.js
@@ -0,0 +1,13 @@
+function calc_power() {
+  // saving user inputs into variables.
+  const number1 = Number(document.getElementById("user_num_1").value);
+  const number2 = Number(document.getElementById("user_num_2").value);
+  const paragraph = document.querySelector("p");
+  let result = number1;
+  // A loop that multiplies the number with itself n times
+  for (let counter = 0; counter < number2 - 1; counter++)
+  {
+      result *= number1;
+  }
+    paragraph.textContent = `${number1} to the power of ${number2} is ${result}.`;
+}
