40b27d46b884ce9185263ace6a6d4f597d35b8f5
Lena Blechmann <lenablechmann@gmail.com>
2020-12-28 11:16:44 +0100

    layout loop practice files

---
+++ javascript_fundamentals/4_Basics/scripts/loops.js
@@ -0,0 +1,5 @@
+// coding along https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Looping_code
+
+function logLoop() {
+    document.getElementById("js-loop-creation").innerHTML = newArray;
+}
