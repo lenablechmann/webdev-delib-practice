95027a93e64b1d09f3fc4ac31714d12c8e043d79
Lena Blechmann <lenablechmann@gmail.com>
2020-12-28 16:31:15 +0100

    Add even and prime numbers functions

--- javascript_fundamentals/4_Basics/scripts/loops.js
+++ javascript_fundamentals/4_Basics/scripts/loops.js
@@ -24,7 +24,6 @@ function searchNames() {
     "Existence:98305923",
   ];
   userInput = prompt("Which contact are you looking for?").toLowerCase();
-  console.log(userInput);
   for (let counter = 0; counter < contacts.length; counter++) {
     // Splitting contacts into two parts [0] and [1] on :
     let splitter = contacts[counter].split(":");
@@ -38,4 +37,49 @@ function searchNames() {
   }
   // print if nothing was found
   document.getElementById("js-loop-creation").innerHTML = "No such contact exists";
+}
+
+function putEvenNumbers(){
+  // coding along https://javascript.info/while-for
+  // Use the for loop to output even numbers from 2 to 10.
+  let text = "Even numbers from 2 to 10 are: ";
+
+  for (let i = 2; i <= 10; i++) {
+    if (i % 2 === 0){
+      text += i + " ";
+    }
+  }
+  document.getElementById("js-loop-creation").innerHTML = text; 
+}
+
+function promptUser(){
+  // coding along https://javascript.info/while-for
+  // Write a loop which prompts for a number greater than 100.
+  // If the visitor enters another number – ask them to input again.
+  let userNum;
+    do {
+      userNum = parseInt(prompt("Enter any number greater than 100."));
+  } while (userNum < 100);
+
+  document.getElementById("js-loop-creation").innerHTML = `Well done.`; 
+}
+
+function getEvenNumbers(){
+  // coding along https://javascript.info/while-for
+  // Write the code which outputs prime numbers in the interval from 2 to n
+  const NUMBER = 150;
+  let text = `Prime numbers between 2 and ${NUMBER} are: `;
+
+  // Using labels to jump out of the inner loop
+  next: for (let num = 2; num <= NUMBER; num++){
+    for ( let counter = 2; counter < num; counter++)
+    {
+      if (num % counter === 0 && num !== counter){
+        continue next;
+      }
+    }
+    text += num + " ";
+  }
+
+  document.getElementById("js-loop-creation").innerHTML = text; 
 }
