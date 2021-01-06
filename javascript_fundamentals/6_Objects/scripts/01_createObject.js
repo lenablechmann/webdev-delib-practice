aff767bc9070d664735c6aa4b8cecb5598078426
Lena Blechmann <lenablechmann@gmail.com>
2021-01-06 12:27:16 +0100

    code along deleting properties

--- javascript_fundamentals/6_Objects/scripts/01_createObject.js
+++ javascript_fundamentals/6_Objects/scripts/01_createObject.js
@@ -1,6 +1,7 @@
 // practicing basic object creation and calling
 // objects can have everything: strings, numbers, arrays and even functions(methods)
-const person = {firstName: "Lena",
+// they are keyed collections of data
+const person = {firstName: "Lena", // property (key:value pair)
                 lastName: "Blechmann",
                 age: "32",
                 favColors: ["blue", "navy", "blue jay", "maroon", "firebrick"],
@@ -14,4 +15,22 @@ const person = {firstName: "Lena",
 };
 
 // calling the object + method
-person.personInfo();
+//person.personInfo();
+
+// following https://javascript.info/object
+// objects can be created via two syntaxes
+let something = new Object(); // object constructor
+let anotherThing = {}; // object literal
+
+// adding key value pairs:
+anotherThing = {user: "John",
+                age: "30",
+                adminStatus: true, 
+                "likes birds": false, //multiword keywords gotta be in quotes
+                };
+                
+// removing properties
+delete anotherThing.age;
+
+// accessing multiword property via square brackets [], no dot after object
+console.log(anotherThing["likes birds"]);
