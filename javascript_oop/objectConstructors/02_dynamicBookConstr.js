f09480e2d87cbecbb3a071c9516efac15c07cfff
Lena Blechmann <lenablechmann@gmail.com>
2021-01-14 19:01:44 +0100

    store book obj in an array

--- javascript_oop/objectConstructors/02_dynamicBookConstr.js
+++ javascript_oop/objectConstructors/02_dynamicBookConstr.js
@@ -1,21 +1,26 @@
 // Create a page, where users can create books by filling out forms
 // you'll check em in console log
 
-function book(title, author, yearPublished, description, readStatus){
-    // using "this" keyword to refer to the context of this function
-    this.title = title;
-    this.author = author;
-    this.yearPublished = yearPublished;
-    this.description = description;
-    this.readStatus = readStatus;
-    this.printBookInfo = function(){
-        console.log(`${title} from ${author} published in ${yearPublished} is all about ${description}.`)
-    };
+
+
+document.querySelector("form").onsubmit = function () {
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  storeBooks(title, author)
 }
+console.table(booksCollection);
 
-const bookFirst = new book("Modern C", "King", 1990, "pointers, pointers everywhere", true);
-// prints the whole object, wow
-// console.log(bookFirst);
+function storeBooks(title, author){
+  let booksCollection = [];
+  userAddedBook = new book(title, author);
+  booksCollection.push(userAddedBook);
+};
 
-// calls the function
-bookFirst.printBookInfo();
+function book(title, author) {
+  // using "this" keyword to refer to the context of this function
+  this.title = title;
+  this.author = author;
+  this.printBookInfo = function () {
+    console.log(`You've mentioned ${title} from ${author}.`);
+  };
+}
