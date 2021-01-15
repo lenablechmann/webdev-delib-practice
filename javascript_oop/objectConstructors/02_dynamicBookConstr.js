6891b9dd98ea4502c0d823ecaaa483e8e15d5075
Lena Blechmann <lenablechmann@gmail.com>
2021-01-15 10:20:50 +0100

    find out you can only store strings in local storage

--- javascript_oop/objectConstructors/02_dynamicBookConstr.js
+++ javascript_oop/objectConstructors/02_dynamicBookConstr.js
@@ -1,19 +1,38 @@
+/* Currently there is a problem with the local storage and overall organization of the file
+  I've found out local storage can only store strings,
+  Thus I need to make a
+  local storage prototype for objects with JSON stringify then parse. 
+
+  And also figure out a way how to store data from addEventListener functions, as
+  those are asynchronous...Perhaps I need a separate tracer bullet prototype file
+  where functions send info to each other. */ 
+
 // Create a page, where users can create books by filling out forms
-// you'll check em in console log
+let booksCollection = [];
 
 
+if(!localStorage.getItem('booksCollection')) {
+  // setting up a local storage array, have to stringify with json
+  // unpack it later with
+  // array = localStorage.getItem('myArray');
+  // array = JSON.parse(array);
+
+  localStorage.setItem('booksCollection', JSON.stringify(booksCollection));
+}
 
 document.querySelector("form").onsubmit = function () {
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
-  storeBooks(title, author)
-}
-console.table(booksCollection);
+  storeBooks(title, author);
+};
 
 function storeBooks(title, author){
-  let booksCollection = [];
   userAddedBook = new book(title, author);
-  booksCollection.push(userAddedBook);
+  let booksCollection = localStorage.getItem('booksCollection') || [];
+  // booksCollection = JSON.parse(booksCollection);
+  // booksCollection.push(userAddedBook);
+console.log(typeof booksCollection);
+  console.log(booksCollection);
 };
 
 function book(title, author) {
