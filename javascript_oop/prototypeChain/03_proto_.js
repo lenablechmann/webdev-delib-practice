1c5da41c3fe719db14210b0393b67cfbd633240a
Lena Blechmann <lenablechmann@gmail.com>
2021-01-17 16:40:55 +0100

    add __proto__ practice

--- javascript_oop/prototypeChain/03_proto_.js
+++ javascript_oop/prototypeChain/03_proto_.js
@@ -23,3 +23,41 @@ console.log( rabbit.jumps ); //  should be null, as the rabbit jumps got deleted
 delete animal.jumps;
 
 console.log( rabbit.jumps ); // should be undefined
+
+
+/* TASK 2 
+    Use __proto__ to assign prototypes in a way that 
+    any property lookup will follow the path:
+    pockets → bed → table → head. 
+    For instance, pockets.pen should be 3 (found in table), 
+    and bed.glasses should be 1 (found in head).
+    
+    Answer the question: 
+    is it faster to get glasses as pockets.glasses or head.glasses? 
+    head.
+    My Answer: head.glasses should be faster, since the property is immediately found in the
+    object and you don't have to travel up the prototype chain
+*/
+
+let head = {
+    glasses: 1
+  };
+  
+  let table = {
+    pen: 3,
+    __proto__: head
+  };
+  
+  let bed = {
+    sheet: 1,
+    pillow: 2,
+    __proto__: table
+  };
+  
+  let pockets = {
+    money: 2000,
+    __proto__: bed
+  };
+
+  console.log(pockets.pen); // 3
+  console.log(bed.glasses); // 1
