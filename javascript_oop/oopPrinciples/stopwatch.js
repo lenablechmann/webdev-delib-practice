2fee0741364381c80f35c6d463de3d5810725b7c
Lena Blechmann <lenablechmann@gmail.com>
2021-02-09 09:33:28 +0100

    write a lil oop stopwatch in js to practice the basic concepts

--- javascript_oop/oopPrinciples/stopwatch.js
+++ javascript_oop/oopPrinciples/stopwatch.js
@@ -1,31 +1,58 @@
 // oop practice with a stopwatch
-// needs to have a start, a stop and a reset
+// needs to have a start, a stop and a reset methos
+// a property named duration. If you start again after starting
+// it should throw an error
 
 function createStopwatch () {
     // setting all the private values
     let timer = 0;
     let difference = 0;
     let startingTime = 0;
+    let running = false;
 
     this.start = function start () {
-        startingTime = Date.now()
+        // only changes starting time, if it's not running yet
+        if (!running) {
+            startingTime = Date.now();
+            running = true;
+            console.log("started");
+        }
+        else {
+            throw new Error("Stopwatch is already running. Stop it first.")
+        }
     };
 
     this.stop = function stop () {
-        console.log("off to a good stop");
+        if (running){
+            // get difference from start to now
+            difference = Date.now() - startingTime;
+            timer += difference;
+            running = false;
+            console.log("stopped");
+        }
+        else {
+            throw new Error("Stopwatch is already stopped. Start it first.")
+        }
     };
 
     this.reset = function reset () {
-        console.log("reset ");
+        running = false;
+        timer = 0;
+        difference = 0;
+        startingTime = 0;
     }
 
-    return {start, stop, reset, duration};
+    return {start,
+            stop,
+            reset,
+            duration: timer
+        };
 };
 
 const stopwatch = createStopwatch();
 
 // testing methods
-stopwatch.start();
-stopwatch.stop();
-stopwatch.reset();
+ stopwatch.start();
+ stopwatch.stop();
+// stopwatch.reset();
 console.log(stopwatch.duration);
